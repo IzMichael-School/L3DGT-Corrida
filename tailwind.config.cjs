84f88f7862c38b54f2b9ce5e551cb419fc86d308
IzMichael <michael@izmichael.com>
2024-02-07 12:34:55 +1300

    Import IzSK Template

---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{html,js,svelte,ts}'],
+    theme: {
+        extend: {
+            height: {
+                screen: '100dvh'
+            },
+        },
+        fontFamily: {
+            sans: ['Lexend', 'Rubik', 'sans-serif'],
+            mono: ['Spline Sans Mono', 'monospace'],
+
+            lexend: ['Lexend'],
+            splinesans: ['Spline Sans Mono'],
+            rubik: ['Rubik'],
+        },
+    },
+    plugins: [],
+}
