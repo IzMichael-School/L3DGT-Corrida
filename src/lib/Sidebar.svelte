1d27666970a40e3d88539afde4278464878a3489
IzMichael <michael@izmichael.com>
2024-02-23 15:10:02 +1300

    Build App Framework

---
+++ src/lib/Sidebar.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+    import { applyAction, enhance } from '$app/forms';
+    import { pb } from '$lib/pocketbase';
+    import { page } from '$app/stores';
+</script>
+
+<div class="flex h-full w-64 flex-col border-r-2 border-r-gray-200 bg-white p-5">
+    <div class="mb-5 flex w-full flex-row items-center justify-center">
+        <img src="/assets/icons/checkbox.svg" alt="Checkbox Icon" class="mr-3 mt-1 h-8 w-8" />
+        <h1 class="font-jura text-4xl font-bold text-brand">Voix</h1>
+    </div>
+
+    <a class="nav-button" class:active={$page.route.id?.startsWith('/app/surveys')} href="/app/surveys">
+        <img src="/assets/icons/list-numbers.svg" alt="List with Numbers Icon" />
+        <p>Surveys</p>
+    </a>
+
+    <a class="nav-button" class:active={$page.route.id?.startsWith('/app/rooms')} href="/app/rooms">
+        <img src="/assets/icons/users-group.svg" alt="Group of People Icon" />
+        <p>Rooms</p>
+    </a>
+
+    <a class="nav-button mt-5" class:active={$page.route.id?.startsWith('/app/answers')} href="/app/answers">
+        <img src="/assets/icons/history-toggle.svg" alt="Clock with partially dashed lines Icon" />
+        <p>Past Answers</p>
+    </a>
+
+    <a class="nav-button" class:active={$page.route.id?.startsWith('/app/profile')} href="/app/profile">
+        <img src="/assets/icons/user.svg" alt="Person with Cog Icon" />
+        <p>Profile</p>
+    </a>
+
+    <a class="nav-button" href="/">
+        <img src="/assets/icons/door-enter.svg" alt="Door with Arrow Pointing Inwards Icon" />
+        <p>Join Room</p>
+    </a>
+
+    <form
+        method="POST"
+        action="/logout"
+        use:enhance={() => {
+            return async ({ result }) => {
+                pb.authStore.clear();
+                await applyAction(result);
+            };
+        }}
+        class="contents"
+    >
+        <button class="nav-button mt-5">
+            <img src="/assets/icons/logout.svg" alt="Leaving through Door Icon" />
+            <p>Log Out</p>
+        </button>
+    </form>
+</div>
+
+<style>
+    .nav-button {
+        @apply mb-2 flex flex-row items-center justify-start rounded-lg border p-2 font-bold shadow;
+    }
+    .nav-button:hover {
+        @apply bg-zinc-100;
+    }
+    .nav-button.active {
+        @apply bg-brand/20;
+    }
+    .nav-button img {
+        @apply mr-2 h-5 w-5;
+    }
+</style>
---
+++ src/routes/app/+layout.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import Sidebar from '$lib/Sidebar.svelte';
+    import { pb } from '$lib/pocketbase';
+    import { goto } from '$app/navigation';
+    import { browser } from '$app/environment';
+
+    $: if (!pb.authStore.isValid && browser) {
+        goto('/login');
+    }
+</script>
+
+<div class="flex h-screen max-h-screen w-full flex-row items-center justify-start overflow-hidden bg-stone-100">
+    <Sidebar />
+    <div class="flex h-full max-h-full flex-1 flex-col overflow-auto p-3">
+        <slot />
+    </div>
+</div>
+
+<style>
+</style>
