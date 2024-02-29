4680a47d713ecda900bb75bacfdddc45efae41a3
IzMichael <michael@izmichael.com>
2024-02-29 17:58:38 +1300

    Add API layer for Room DB

---
+++ src/routes/app/rooms/+server.ts
@@ -0,0 +1,23 @@
+import { error, json } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+import { customAlphabet } from 'nanoid';
+const nanoid = customAlphabet('ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890', 7);
+
+export const GET: RequestHandler = async ({ locals }) => {
+    const rooms = await locals.pb.collection('rooms').getFullList(200, {
+        filter: `owner.id = "${locals.user?.id}"`,
+    });
+    return json({ rooms });
+};
+
+export const POST: RequestHandler = async ({ locals }) => {
+    if (!locals.user?.id) return error(401, 'You must be logged in to complete this action.');
+
+    const room = await locals.pb.collection('rooms').create({
+        title: 'Untitled Room',
+        code: nanoid(),
+        status: 'waiting',
+        owner: locals.user.id,
+    });
+    return json(room);
+};
