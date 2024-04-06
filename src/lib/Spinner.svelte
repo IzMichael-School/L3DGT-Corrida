5d1bb2d9c80014867b01c536310ad20287f158e8
IzMichael <michael@izmichael.com>
2024-04-06 21:44:30 +1300

    New Spinner

--- src/lib/Spinner.svelte
+++ src/lib/Spinner.svelte
@@ -32,35 +32,70 @@
 
 <style>
     .loader {
-        width: 128px;
-        height: 128px;
-        display: inline-block;
-        position: relative;
-    }
-    .loader::after,
-    .loader::before {
-        content: '';
-        box-sizing: border-box;
-        width: 128px;
-        height: 128px;
-        border: 8px solid;
-        @apply border-brand-green;
-        position: absolute;
-        left: 0;
-        top: 0;
-        animation: rotation 2s ease-in-out infinite alternate;
-    }
-    .loader::after {
-        @apply border-brand;
-        animation-direction: alternate-reverse;
+        animation: rotate 1s infinite;
+        height: 50px;
+        width: 50px;
     }
 
-    @keyframes rotation {
+    .loader:before,
+    .loader:after {
+        border-radius: 50%;
+        content: '';
+        display: block;
+        height: 20px;
+        width: 20px;
+    }
+    .loader:before {
+        animation: ball1 1s infinite;
+        background-color: #297b4e;
+        box-shadow: 30px 0 0 #654597;
+        margin-bottom: 10px;
+    }
+    .loader:after {
+        animation: ball2 1s infinite;
+        background-color: #654597;
+        box-shadow: 30px 0 0 #297b4e;
+    }
+
+    @keyframes rotate {
         0% {
-            transform: rotate(0deg);
+            transform: rotate(0deg) scale(0.8);
+        }
+        50% {
+            transform: rotate(360deg) scale(1.2);
         }
         100% {
-            transform: rotate(360deg);
+            transform: rotate(720deg) scale(0.8);
+        }
+    }
+
+    @keyframes ball1 {
+        0% {
+            box-shadow: 30px 0 0 #654597;
+        }
+        50% {
+            box-shadow: 0 0 0 #654597;
+            margin-bottom: 0;
+            transform: translate(15px, 15px);
+        }
+        100% {
+            box-shadow: 30px 0 0 #654597;
+            margin-bottom: 10px;
+        }
+    }
+
+    @keyframes ball2 {
+        0% {
+            box-shadow: 30px 0 0 #297b4e;
+        }
+        50% {
+            box-shadow: 0 0 0 #297b4e;
+            margin-top: -20px;
+            transform: translate(15px, 15px);
+        }
+        100% {
+            box-shadow: 30px 0 0 #297b4e;
+            margin-top: 0;
         }
     }
 </style>
